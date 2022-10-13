c50becc0d88978e4de85ad7626bf80fa61aeb4f0
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2022-10-13 18:16:17 -0300

    Ensures that the logs folder is created.

--- src/main/java/ar/edu/unrc/exa/dc/mfis/event_logger/LocalLogging.java
+++ src/main/java/ar/edu/unrc/exa/dc/mfis/event_logger/LocalLogging.java
@@ -1,22 +1,26 @@
 package ar.edu.unrc.exa.dc.mfis.event_logger;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.logging.*;
 
 public class LocalLogging {
 
-    public static Logger getLogger(Class<?> forClass) {
-        return getLogger(forClass, Level.ALL);
-    }
+    public static Logger getLogger(Class<?> forClass) {return getLogger(forClass, Level.ALL); }
     public static Logger getLogger(Class<?> forClass, Level loggingLevel) {
-        Logger logger = Logger.getLogger(forClass.getName());;
+        Logger logger = Logger.getLogger(forClass.getName());
+        final Path logsFolder = Paths.get("", "logs");
         try{
+            Files.createDirectories(logsFolder);
             Handler[] handlers = logger.getHandlers();
             Arrays.stream(handlers).forEach(logger::removeHandler);
             //Assigning handlers to LOGGER object
             logger.addHandler(new CoolConsoleHandler());
-            FileHandler fileHandler = new FileHandler("logs/" + forClass.getName() +  ".log");
+            FileHandler fileHandler = new FileHandler(Paths.get(logsFolder.toString(), forClass.getName() +  ".log").toString());
             logger.addHandler(fileHandler);
             //Setting levels to handlers and LOGGER
             logger.setLevel(loggingLevel);
